628a800ed4162c35aadd4f90c8a175f1b13691e7
Korolenko Daria <admem75213@gmail.com>
2024-04-21 19:15:59 +0300

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,30 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      src: "/src",
-      components: "/src/components",
-      pages: "/src/pages",
-      assets: "/src/assets",
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
     },
-  },
-  base: "/your_camper/",
+    root: "src",
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync("./src/*.html"),
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+          entryFileNames: "commonHelpers.js",
+        },
+      },
+      outDir: "../dist",
+    },
+    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+  };
 });
